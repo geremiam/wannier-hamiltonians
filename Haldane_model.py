69d910e84496a1374d65da95d365a19d8b35fdde
Geremia Massarelli <geemassa@gmail.com>
2021-05-19 20:20:12 -0400

    Errors fixed in Haldane_model.py.
    There were two: Missing parentheses got the order of operations wrong. Also, axis arguments for the roll() function were wrong.

--- Haldane_model.py
+++ Haldane_model.py
@@ -3,7 +3,74 @@ from numpy import sqrt, cos, sin
 import misc as mi
 import matplotlib.pyplot as plt
 
-def HaldaneHamiltonian(k, params, periodic=False):
+def find_gaps(evals):
+    
+    gaps_array = evals[..., 1:] - evals[..., :-1]
+    
+    gaps_min = np.amin(gaps_array, axis=tuple(range(gaps_array.ndim-1)))
+    
+    return np.squeeze(gaps_min)
+
+def link_variable(evecs, verbose=False):
+    ''' Calculate Wilson line elements between adjacent momentum points using evecs_occ. 
+    Last two dimensions of evecs_occ are orbitals and bands, previous ones are momentum
+    directions. If unitary==True, the purely "unitary part" of the Wilson line elements 
+    are returned
+    '''
+    
+    # Last two dimensions are orbitals and bands, previous ones are momentum
+    Nbands = evecs.shape[-1]
+    k_shape = evecs.shape[:-2]
+    D = len(k_shape)
+    
+    if verbose:
+        mi.sprint('Nbands',Nbands)
+        mi.sprint('D',D)
+    
+    # Create return array
+    U = np.zeros((D,) + k_shape + (Nbands,), complex)
+    
+    for d in range(D):
+        U_temp = np.sum( evecs.conj() * np.roll(evecs, -1, axis=d), axis=-2 )
+        U_temp *= 1. / np.abs(U_temp)
+        
+        U[d,...] = U_temp
+    
+    return U
+
+def Chern_fromlinkvar(linkvar, imagtol=1.e-14):
+    
+    # Only makes sense for two spatial dimensions
+    assert linkvar.ndim==4
+    assert linkvar.shape[0]==2
+    
+    factor1 = linkvar[0,...]
+    factor2 = np.roll(linkvar[1,...], -1, axis=0)
+    factor3 = np.roll(linkvar[0,...], -1, axis=1)
+    factor4 = linkvar[1,...]
+    
+    F12 = np.log( factor1*factor2 / (factor3*factor4) ) / 1.j
+    
+    F12_imag = np.imag(F12)
+    F12_imag_max = np.amax(np.abs(F12_imag))
+    
+    if F12_imag_max>imagtol:
+        print('WARNING: In Chern_fromlinkvar(), imaginary parts were larger than tolerance. Largest is {}'.format(F12_imag_max))
+    else:
+        F12 = np.real(F12)
+    
+    C = np.sum( F12, axis=(0,1) ) / (2.*np.pi)
+    
+    return C
+
+def Chern(evecs, imagtol=1.e-14, verbose=False):
+    U = link_variable(evecs, verbose=verbose)
+    C = Chern_fromlinkvar(U, imagtol=imagtol)
+    return C
+
+# #######################################################################################
+
+def HaldaneHamiltonian(k, params, periodic=True):
     t1 = params['t1']
     t2 = params['t2']
     phi = params['phi']
@@ -43,100 +110,6 @@ def HaldaneHamiltonian(k, params, periodic=False):
     
     return mat
 
-def find_gaps(Hamiltonian, params, Nlist):
-    
-    linspaces_list = []
-    for N in Nlist:
-        linspaces_list.append( np.linspace(-0.5, 0.5, N, endpoint=False) )
-    
-    
-    k = np.array( np.meshgrid(*linspaces_list, indexing='ij') )
-    
-    ham = HaldaneHamiltonian(k, params)
-    
-    evals = np.linalg.eigvalsh(ham)
-    
-    Ngaps = evals.shape[-1] - 1
-    
-    gaps_array = evals[..., 1:] - evals[..., :-1]
-    
-    gaps_min = np.amin(gaps_array, axis=tuple(range(gaps_array.ndim-1)))
-    
-    return np.squeeze(gaps_min)
-
-def find_gaps_Hal():
-    params = {'t1':1., 't2':0.3, 'phi':0., 'M':0. }
-    Nlist = [200, 200]
-    
-    N_M = 31
-    N_phi = 31
-    M_array = params['t2'] * np.linspace(-10., 10., N_M, endpoint=True)
-    phi_array = np.linspace(-np.pi, np.pi, N_phi, endpoint=True)
-    
-    gap_array = np.zeros(M_array.shape + phi_array.shape)
-    
-    for M_idx, M in enumerate(M_array):
-        params['M'] = M
-        for phi_idx, phi in enumerate(phi_array):
-            params['phi'] = phi
-            gap_array[M_idx, phi_idx] = find_gaps(HaldaneHamiltonian, params, Nlist)
-    
-    fig, ax = plt.subplots()
-    ax.contourf(M_array, phi_array, gap_array, 25)
-    plt.show()
-
-def link_variable(evecs, verbose=True):
-    ''' Calculate Wilson line elements between adjacent momentum points using evecs_occ. 
-    Last two dimensions of evecs_occ are orbitals and bands, previous ones are momentum
-    directions. If unitary==True, the purely "unitary part" of the Wilson line elements 
-    are returned
-    '''
-    
-    # Last two dimensions are orbitals and bands, previous ones are momentum
-    Nbands = evecs.shape[-1]
-    k_shape = evecs.shape[:-2]
-    D = len(k_shape)
-    
-    if verbose:
-        mi.sprint('Nbands',Nbands)
-        mi.sprint('D',D)
-    
-    # Create return array
-    U = np.zeros((D,) + k_shape + (Nbands,), complex)
-    
-    for d in range(D):
-        U_temp = np.sum( evecs.conj() * np.roll(evecs, -1, axis=d), axis=-2 )
-        U_temp *= 1. / np.abs(U_temp)
-        
-        U[d,...] = U_temp
-    
-    return U
-
-def Chern(linkvar, imagtol=1.e-15):
-    
-    # Only makes sense for two spatial dimensions
-    assert linkvar.ndim==4
-    assert linkvar.shape[0]==2
-    
-    factor1 = U[0,...]
-    factor2 = np.roll(U[1,...], -1, 1)
-    factor3 = np.roll(U[0,...], -1, 2)
-    factor4 = U[1,...]
-    
-    F12 = np.log( factor1*factor2 / (factor3*factor4) ) / 1.j
-    
-    F12_imag = np.imag(F12)
-    F12_imag_max = np.amax(np.abs(F12_imag))
-    
-    if F12_imag_max>imagtol:
-        print('WARNING: In Chern(), imaginary parts were larger than tolerance. Largest is {}'.format(F12_imag_max))
-    else:
-        F12 = np.real(F12)
-    
-    C = np.sum( F12, axis=(0,1) ) / 2.*np.pi
-    
-    return C
-
 def check_equiv_of_hamiltonians():
     params = {'t1':1., 't2':0.3, 'phi':np.pi/2., 'M':0. }
     N1, N2 = 1000, 1100
@@ -158,24 +131,40 @@ def check_equiv_of_hamiltonians():
     
     return
 
-if __name__ == "__main__":
-    np.set_printoptions(linewidth=750)
+def plot_Chern_Haldane():
+    params = {'t1':1., 't2':0.3, 'phi':0., 'M':0. }
     
-    params = {'t1':1., 't2':0.3, 'phi':np.pi/2., 'M':0. }
-    N1, N2 = 1000, 1100
-    
-    gap = find_gaps(HaldaneHamiltonian, params, [N1,N2])
-    mi.sprint('gap',gap)
-    
+    N1 = 30
+    N2 = N1+1
     k1 = np.linspace(-0.5, 0.5, N1, endpoint=False)
     k2 = np.linspace(-0.5, 0.5, N2, endpoint=False)
     k = np.array(np.meshgrid(k1,k2, indexing='ij'))
-    ham = HaldaneHamiltonian(k, params)
     
-    evals, evecs = np.linalg.eigh(ham)
+    N_M = 100
+    N_phi = N_M+1
+    M_array = params['t2'] * np.linspace(-7., 7., N_M, endpoint=True)
+    phi_array = np.linspace(-np.pi, np.pi, N_phi, endpoint=True)
     
-    U = link_variable(evecs)
-    mi.sprint('U.shape', U.shape)
+    gap_array = np.zeros(M_array.shape + phi_array.shape)
+    Chern_array = np.zeros(M_array.shape + phi_array.shape + (2,))
     
-    C = Chern(U)
-    mi.sprint('C', C)
+    for M_idx, M in enumerate(M_array):
+        params['M'] = M
+        for phi_idx, phi in enumerate(phi_array):
+            params['phi'] = phi
+            
+            evals, evecs = np.linalg.eigh( HaldaneHamiltonian(k, params) )
+            
+            gap_array[M_idx, phi_idx] = find_gaps(evals)
+            Chern_array[M_idx, phi_idx,:] = Chern(evecs)
+    
+    fig, ax = plt.subplots(1,3, sharex=True, sharey=True)
+    ax[0].pcolormesh(phi_array, M_array, gap_array, shading='nearest', cmap='magma')
+    ax[1].pcolormesh(phi_array, M_array, Chern_array[...,0], shading='nearest', cmap='coolwarm')
+    ax[2].pcolormesh(phi_array, M_array, Chern_array[...,1], shading='nearest', cmap='coolwarm')
+    plt.show()
+
+if __name__ == "__main__":
+    np.set_printoptions(linewidth=750)
+    
+    plot_Chern_Haldane()
