01f555f875fb68fb0797927668afc0ff3261675c
Geremia Massarelli <geemassa@gmail.com>
2021-05-20 19:03:26 -0400

    Routines for computing Chern number were put into a separate module. Tentatively, functionality for paraunitary evecs was added.

---
+++ Chern_toolbox.py
@@ -0,0 +1,67 @@
+import numpy as np
+import misc as mi
+
+
+def link_variable(evecs, metric=None, verbose=False):
+    ''' Calculate Wilson line elements between adjacent momentum points using evecs_occ. 
+    Last two dimensions of evecs_occ are orbitals and bands, previous ones are momentum
+    directions. If unitary==True, the purely "unitary part" of the Wilson line elements 
+    are returned
+    '''
+    
+    # Last two dimensions are orbitals and bands, previous ones are momentum
+    Nbands = evecs.shape[-1]
+    k_shape = evecs.shape[:-2]
+    D = len(k_shape)
+    
+    if verbose:
+        print('Nbands = {}'.format(Nbands))
+        print('D = {}'.format(D))
+    
+    # Create return array
+    U = np.zeros((D,) + k_shape + (Nbands,), complex)
+    
+    for d in range(D):
+        if metric is None:
+            U_temp = np.sum( evecs.conj() * np.roll(evecs, -1, axis=d), axis=-2 )
+        else:
+            norms = np.sum( evecs.conj() * (metric @ evecs), axis=-2 )
+            U_temp = np.sum( evecs.conj() * np.roll(evecs, -1, axis=d), axis=-2 ) / norms
+        
+        U_temp *= 1. / np.abs(U_temp)
+        
+        U[d,...] = U_temp
+    
+    return U
+
+def Chern_fromlinkvar(linkvar, imagtol=1.e-14):
+    
+    # Only makes sense for two spatial dimensions
+    assert linkvar.ndim==4
+    assert linkvar.shape[0]==2
+    
+    factor1 = linkvar[0,...]
+    factor2 = np.roll(linkvar[1,...], -1, axis=0)
+    factor3 = np.roll(linkvar[0,...], -1, axis=1)
+    factor4 = linkvar[1,...]
+    
+    F12 = np.log( factor1*factor2 / (factor3*factor4) ) / 1.j
+    
+    F12_imag = np.imag(F12)
+    F12_imag_max = np.amax(np.abs(F12_imag))
+    
+    if F12_imag_max>imagtol:
+        print('WARNING: In Chern_fromlinkvar(), imaginary parts were larger than tolerance. Largest is {}'.format(F12_imag_max))
+    else:
+        F12 = np.real(F12)
+    
+    C = np.sum( F12, axis=(0,1) ) / (2.*np.pi)
+    
+    return C
+
+# #######################################################################################
+
+def Chern(evecs, imagtol=1.e-14, verbose=False):
+    U = link_variable(evecs, verbose=verbose)
+    C = Chern_fromlinkvar(U, imagtol=imagtol)
+    return C
--- Haldane_model.py
+++ Haldane_model.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import multiprocessing
 import joblib
 
+import Chern_toolbox as CT
+
 def find_gaps(evals):
     
     gaps_array = evals[..., 1:] - evals[..., :-1]
@@ -13,63 +15,6 @@ def find_gaps(evals):
     gaps_min = np.amin(gaps_array, axis=tuple(range(gaps_array.ndim-1)))
     
     return np.squeeze(gaps_min)
-
-def link_variable(evecs, verbose=False):
-    ''' Calculate Wilson line elements between adjacent momentum points using evecs_occ. 
-    Last two dimensions of evecs_occ are orbitals and bands, previous ones are momentum
-    directions. If unitary==True, the purely "unitary part" of the Wilson line elements 
-    are returned
-    '''
-    
-    # Last two dimensions are orbitals and bands, previous ones are momentum
-    Nbands = evecs.shape[-1]
-    k_shape = evecs.shape[:-2]
-    D = len(k_shape)
-    
-    if verbose:
-        mi.sprint('Nbands',Nbands)
-        mi.sprint('D',D)
-    
-    # Create return array
-    U = np.zeros((D,) + k_shape + (Nbands,), complex)
-    
-    for d in range(D):
-        U_temp = np.sum( evecs.conj() * np.roll(evecs, -1, axis=d), axis=-2 )
-        U_temp *= 1. / np.abs(U_temp)
-        
-        U[d,...] = U_temp
-    
-    return U
-
-def Chern_fromlinkvar(linkvar, imagtol=1.e-14):
-    
-    # Only makes sense for two spatial dimensions
-    assert linkvar.ndim==4
-    assert linkvar.shape[0]==2
-    
-    factor1 = linkvar[0,...]
-    factor2 = np.roll(linkvar[1,...], -1, axis=0)
-    factor3 = np.roll(linkvar[0,...], -1, axis=1)
-    factor4 = linkvar[1,...]
-    
-    F12 = np.log( factor1*factor2 / (factor3*factor4) ) / 1.j
-    
-    F12_imag = np.imag(F12)
-    F12_imag_max = np.amax(np.abs(F12_imag))
-    
-    if F12_imag_max>imagtol:
-        print('WARNING: In Chern_fromlinkvar(), imaginary parts were larger than tolerance. Largest is {}'.format(F12_imag_max))
-    else:
-        F12 = np.real(F12)
-    
-    C = np.sum( F12, axis=(0,1) ) / (2.*np.pi)
-    
-    return C
-
-def Chern(evecs, imagtol=1.e-14, verbose=False):
-    U = link_variable(evecs, verbose=verbose)
-    C = Chern_fromlinkvar(U, imagtol=imagtol)
-    return C
 
 # #######################################################################################
 
@@ -157,7 +102,7 @@ def plot_Chern_Haldane(parallelize=False, plot=False):
         evals, evecs = np.linalg.eigh( HaldaneHamiltonian(k, params) )
         
         gaps = find_gaps(evals)
-        Ch = Chern(evecs)
+        Ch = CT.Chern(evecs)
         
         return gaps, Ch
     
@@ -185,7 +130,7 @@ def plot_Chern_Haldane(parallelize=False, plot=False):
                 evals, evecs = np.linalg.eigh( HaldaneHamiltonian(k, params) )
                 
                 gap_array[M_idx, phi_idx] = find_gaps(evals)
-                Chern_array[M_idx, phi_idx,:] = Chern(evecs)
+                Chern_array[M_idx, phi_idx,:] = CT.Chern(evecs)
     
     mi.sprint('gap_array.shape', gap_array.shape)
     mi.sprint('Chern_array.shape', Chern_array.shape)
